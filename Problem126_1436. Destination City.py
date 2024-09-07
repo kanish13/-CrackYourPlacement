137f14fb97650b3ec85e531b53ce3de97a19ef80
Kanish <111358462+kanish13@users.noreply.github.com>
2024-09-07 21:30:18 +0530

    Create Problem126_1436. Destination City.py

---
+++ Problem126_1436. Destination City.py
@@ -0,0 +1,9 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        s=set()
+
+        for i in paths:
+            s.add(i[0])
+        for i in paths:
+            if i[1] not in s:
+                return i[1]
